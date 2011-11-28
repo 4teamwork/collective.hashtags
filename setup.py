9ed54ab88b53c2c7ee18cbd107e2be4d0d43b3dc
Jonas Baumann <jone@jone.ch>
2011-11-28 20:48:41 +0100

    Update package metadata.

--- setup.py
+++ setup.py
@@ -5,34 +5,39 @@ version = '1.0'
 
 tests_require = [
     'plone.app.testing',
-]
+    ]
 
 setup(name='collective.hashtags',
       version=version,
       description="Allows to tag objects by hashtags in richtext-fields.",
-      long_description=open("README.rst").read() + "\n" +
-                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      long_description=open("README.rst").read() + "\n" + \
+          open(os.path.join("docs", "HISTORY.txt")).read(),
+
       # Get more strings from
       # http://pypi.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
         "Framework :: Plone",
         "Programming Language :: Python",
         ],
-      keywords='',
-      author='Julian Infanger',
-      author_email='info@4teamwork.ch',
+
+      keywords='plone collective hashtags',
+      author='4teamwork GmbH',
+      author_email='mailto:info@4teamwork.ch',
       url='https://github.com/4teamwork/collective.hashtags',
-      license='GPL',
+      license='GPL2',
+
       packages=find_packages(exclude=['ez_setup']),
       namespace_packages=['collective'],
       include_package_data=True,
       zip_safe=False,
+
       install_requires=[
-          'setuptools',
-          'Plone',
-      ],
+        'setuptools',
+        'Plone',
+        ],
       tests_require=tests_require,
       extras_require=dict(tests=tests_require),
+
       entry_points="""
       # -*- Entry points: -*-
       [z3c.autoinclude.plugin]
